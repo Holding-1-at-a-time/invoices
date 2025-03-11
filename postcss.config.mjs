ec0696acbc465839580d5574b5ffd48c77bd5e42
holding-1-at-a-time <rromerojr1@gmail.com>
2025-03-11 17:00:12 -0500

    added

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,29 +1,10 @@
-/**
- * The PostCSS configuration.
- *
- * @param { import('postcss-load-config').Options } options - The options for the PostCSS configuration.
- * @returns { import('postcss').Config } The PostCSS configuration.
- */
-export default function postcssConfig(options: import('postcss-load-config').Options): import('postcss').Config {
-  return {
-    plugins: [
-      tailwindcss,
-      autoprefixer,
-      tailwindcssAnimate,
-      postcssImport,
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'postcss-loader',
-          ],
-        },
-      ],
-    },
-  };
-}
+/** @type { import('tailwindcss').Config } */
 
+const config = {
+  plugins: {
+    'autoprefixer': {},
+    '@tailwindcss/postcss': {},
+  }
+};
+
+export default config;
